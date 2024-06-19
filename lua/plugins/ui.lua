cfe699780ceadf72df9a079190e20202c767eff6
Ezra Parker <ezra@cfgrok.com>
2024-06-19 10:55:40 -0700

    feat: increase timeout on notifications

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,11 @@
 return {
+  -- Increase timeout on notifications
+  {
+    "nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
   -- Disable treesitter-context by default -- manually enable to use
   {
     "nvim-treesitter-context",
