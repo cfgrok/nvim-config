a07805cd083bd4d19239fa507fa30cf9c46ee644
Ezra Parker <ezra@cfgrok.com>
2024-06-19 11:02:45 -0700

    feat: lualine configuration
    - Customize display of filetype
    - Add conditional display of file format & encoding

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,34 @@
 return {
+  -- Lualine customization
+  {
+    "lualine.nvim",
+    opts = function(_, opts)
+      table.remove(opts.sections.lualine_c, 3)
+      table.insert(opts.sections.lualine_c, 3, {
+        "filetype",
+        separator = "",
+        padding = { left = 1, right = 0 },
+        icon = { align = "right" },
+      })
+      table.insert(opts.sections.lualine_c, 4, {
+        "fileformat",
+        separator = "",
+        icons_enabled = false,
+        color = "lualine_a_replace",
+        cond = function()
+          return vim.bo.fileformat ~= "unix"
+        end,
+      })
+      table.insert(opts.sections.lualine_c, 5, {
+        "encoding",
+        separator = "",
+        color = "lualine_a_replace",
+        cond = function()
+          return vim.bo.fileencoding ~= "utf-8"
+        end,
+      })
+    end,
+  },
   -- Increase timeout on notifications
   {
     "nvim-notify",
