16c75b00c0e40a6124e7a1ccd1457516db5df267
Ezra Parker <ezra@cfgrok.com>
2024-06-20 12:37:35 -0700

    feat: add more options settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.lazygit_config = false
-vim.g.loaded_perl_provider = 0
-vim.o.mouse = ""
-vim.o.wrap = true
+local vg = vim.g
+local vo = vim.o
+
+vg.lazygit_config = false
+-- WARNING: investigate issue with prettier -- conform healthcheck reports:
+-- WARNING prettier unavailable: Condition failed
+vg.lazyvim_prettier_needs_config = false
+vg.loaded_perl_provider = 0
+
+vo.mouse = ""
+vo.scrolloff = 2
+vo.signcolumn = "yes:2"
+vo.timeoutlen = 400
