e8e9c07302bccc63b596fd2fed6b740e207ec870
Ezra Parker <ezra@cfgrok.com>
2024-06-21 18:50:45 -0700

    feat: replace ts-autotag with vim-closetag

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "bufferline.nvim", enabled = false },
   { "catppuccin", enabled = false },
   { "mini.pairs", enabled = false },
+  { "nvim-ts-autotag", enabled = false },
 }
---
+++ lua/plugins/vim-closetag.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "alvan/vim-closetag",
+    event = "VeryLazy",
+    init = function() vim.g.closetag_filetypes = "cf,eruby,html" end,
+  },
+}
