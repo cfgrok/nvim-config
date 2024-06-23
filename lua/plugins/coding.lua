b756dbcb7c3f3e6c280f1aa4bf8d8f5c072933a2
Ezra Parker <ezra@cfgrok.com>
2024-06-23 15:31:00 -0700

    feat: set up snippets for erb/cf files

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -74,4 +74,15 @@ return {
       })
     end,
   },
+  -- Add snippet filetypes
+  {
+    "nvim-snippets",
+    event = "VeryLazy",
+    opts = {
+      extended_filetypes = {
+        cf = { "html", "javascript" },
+        eruby = { "html" },
+      },
+    },
+  },
 }
