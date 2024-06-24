9186cf28af2359b611332dc404efbf24b43a1d3b
Ezra Parker <ezra@cfgrok.com>
2024-06-24 16:39:22 -0700

    docs: add comments regarding plugin candidates

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -6,4 +6,29 @@ return {
   { "persistence.nvim", enabled = false },
 
   { "tokyonight.nvim", event = "VeryLazy" },
+
+  -- TODO: investigate column manipulation plugins
+  -- { "godlygeek/tabular" }
+  -- { "idbrii/textobj-word-column.vim" }
+
+  -- {
+  --   "jiangmiao/auto-pairs",
+  --   enabled = false,
+  --   init = function()
+  --     vim.g.AutoPairs = {
+  --       ["<%"] = "%>",
+  --       ["<%="] = "%>",
+  --       ["<%#"] = "%>",
+  --       ["("] = ")",
+  --       ["["] = "]",
+  --       ["{"] = "}",
+  --       ["'"] = "'",
+  --       ['"'] = '"',
+  --       ["`"] = "`",
+  --       ["```"] = "```",
+  --       ['"""'] = '"""',
+  --       ["'''"] = "'''",
+  --     }
+  --   end,
+  -- },
 }
