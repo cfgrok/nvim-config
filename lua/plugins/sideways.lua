71e2c3fe4e7ee4fef2d5739d342cc55c6099d3c1
Ezra Parker <ezra@cfgrok.com>
2024-06-25 19:42:34 -0700

    feat: install/configure sideways.vim

---
+++ lua/plugins/sideways.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "andrewradev/sideways.vim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>jh", "<cmd>SidewaysLeft<CR>", desc = "Shift item left" },
+      { "<leader>jl", "<cmd>SidewaysRight<CR>", desc = "Shift item right" },
+      { "<leader>jj", "<cmd>SidewaysJumpLeft<CR>", desc = "Jump item left" },
+      { "<leader>jk", "<cmd>SidewaysJumpRight<CR>", desc = "Jump item right" },
+    },
+  },
+}
