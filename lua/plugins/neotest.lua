41d2332d75b0b2aa1a6b269dbee5be3a97b55f9c
Ezra Parker <ezra@cfgrok.com>
2024-06-27 19:03:38 -0700

    feat: install neotest and vim-test

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "neotest",
+    dependencies = {
+      "nvim-neotest/neotest-vim-test",
+      "vim-test/vim-test",
+      "zidhuss/neotest-minitest",
+    },
+    opts = function(_, opts)
+      opts.adapters = {
+        -- require("neotest-minitest")({
+        --   test_cmd = function()
+        --     return vim.iter({ "rake", "test" }):flatten():totable()
+        --   end,
+        -- }),
+        require("neotest-vim-test")({ allow_file_types = { "ruby" } }),
+      }
+    end,
+  },
+  {
+    "vim-test/vim-test",
+    event = "VeryLazy",
+  },
+}
