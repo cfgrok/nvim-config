e6839793f8691bd5ffc7f685d5c8edf7e43d62b2
Ezra Parker <ezra@cfgrok.com>
2024-07-15 19:19:14 -0700

    feat: configure erb-formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,13 @@ return {
   {
     "conform.nvim",
     opts = {
+      formatters = {
+        erb_format = {
+          inherit = false,
+          command = "erb-format",
+          args = { "--stdin", "--print-width", "120" },
+        },
+      },
       formatters_by_ft = {
         eruby = { "erb_format" },
       },
